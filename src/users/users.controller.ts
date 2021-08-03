46955ee60ac59f92bb5986b30a130ea8a143f00e
Grosty <grostmaster@gmail.com>
2021-08-04 00:37:48 +0300

    added sqlite

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Post('signup')
+  createUser(@Body() { email, password }: CreateUserDto) {
+    return this.userService.create(email, password);
+  }
+
+  @Get(':id')
+  findUser(@Param('id') id: string) {
+    return this.userService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.userService.find(email);
+  }
+}
